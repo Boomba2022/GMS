ef0d60772fde5493f1a497c09f1856545c58bf43
Marijn <85788769+MarijnDM@users.noreply.github.com>
2021-10-02 11:35:32 +0200

    Repo Update

---
+++ GMS_melding/__resource.lua
@@ -0,0 +1,19 @@
+--       Licensed under: AGPLv3        --
+--  GNU AFFERO GENERAL PUBLIC LICENSE  --
+--     Version 3, 19 November 2007     --
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+
+
+
+
+
+
+
+
+
+
+
